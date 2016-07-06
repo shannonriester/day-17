ae951e6a6f4ff4e642de66332702bd3d4d215b6e
Shannon Riester <shannonriester@gmail.com>
2016-07-06 08:45:05 -0500

    touch-ups for display

--- assignments/project/dist/app.js
+++ assignments/project/dist/app.js
@@ -7,15 +7,12 @@ var settings = {
         resultsArr = response.results;
         resultsArr.forEach(function(itemObj) {
             var $itemContainerLink = $('<div class="divContainer" href="#"><main><a href="#" class="aImageIcons"><i class="fa fa-heart icon heartIcon" aria-hidden="true"></i><i class="fa fa-bars icon menuIcon" aria-hidden="true"></i><img class="images" src="#" /></a></main><section class="footer"><a href="#" class="aTitle"></a><section class="lowerFooterSection"><a href="#" class="aShopName"></a><a href="#" class="aPrice"></a></section></section></div>');
+            //
+            // $('.divContainer').hover(function(){
+            //   $('i').toggleClass('inactive');
+            //   // $('.icon').removeClass('inactive');
 
-            $('.divContainer').hover(function(){
-              $('i').toggleClass('inactive');
-              // $('.icon').removeClass('inactive');
-
-
-            });
-
-
+            // });
 
             $itemContainerLink.children('main').children('a').attr('href', itemObj.url);
             $itemContainerLink.children('main').children('a').children('img').attr('src', itemObj.Images[0].url_170x135);

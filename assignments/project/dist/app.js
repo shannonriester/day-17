3f780370ec82288fe018a5b3ae85661fc8c01afd
Shannon Riester <shannonriester@gmail.com>
2016-07-05 21:04:27 -0500

    messing with hovers and togglers

--- assignments/project/dist/app.js
+++ assignments/project/dist/app.js
@@ -6,9 +6,16 @@ var settings = {
     success: function(response) {
         resultsArr = response.results;
         resultsArr.forEach(function(itemObj) {
-            var $itemContainerLink = $('<div class="divContainer" href="#"><main><a href="#" class="aImageIcons"><i class="fa fa-heart icon heartIcon" aria-hidden="true"></i><i class="fa fa-bars icon menuIcon" aria-hidden="true"></i><img src="#" /></a></main><section class="footer"><a href="#" class="aTitle"></a><section class="lowerFooterSection"><a href="#" class="aShopName"></a><a href="#" class="aPrice"></a></section></section></div>');
+            var $itemContainerLink = $('<div class="divContainer" href="#"><main><a href="#" class="aImageIcons"><i class="fa fa-heart icon heartIcon" aria-hidden="true"></i><i class="fa fa-bars icon menuIcon" aria-hidden="true"></i><img class="images" src="#" /></a></main><section class="footer"><a href="#" class="aTitle"></a><section class="lowerFooterSection"><a href="#" class="aShopName"></a><a href="#" class="aPrice"></a></section></section></div>');
 
-            $('i').toggleClass('iconToggleHover');
+            $('.divContainer').hover(function(){
+              $('i').toggleClass('inactive');
+              // $('.icon').removeClass('inactive');
+
+
+            });
+
+
 
             $itemContainerLink.children('main').children('a').attr('href', itemObj.url);
             $itemContainerLink.children('main').children('a').children('img').attr('src', itemObj.Images[0].url_170x135);
@@ -17,10 +24,7 @@ var settings = {
             $itemContainerLink.children('.footer').children('.lowerFooterSection').children('.aPrice').attr('href', itemObj.url).text('$' + itemObj.price);
             $('#mainContainer').prepend($itemContainerLink);
 
-            //for icons
-
-
-            console.log(itemObj);
+            console.log(itemObj.Shop.shop_name);
         });
     }
 };
